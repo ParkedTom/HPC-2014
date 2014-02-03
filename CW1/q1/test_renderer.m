6be5577ef7f51ef5250083a43341c7da02ca6b1e
Tom <tom.ed.parker@gmail.com>
2014-02-03 20:21:14 +0000

    completed q4 and fixed test_renderer to go through all test frames

--- CW1/q1/test_renderer.m
+++ CW1/q1/test_renderer.m
@@ -4,7 +4,7 @@ function [] = test_renderer(renderer)
     w = h;
     c = 0.5:0.2:1.5;
     maxiter = round(2.^(5:0.5:7.5));
-    for i = size(h,2);
+    for i = 1:size(h,2);
         original = render_julia(w(i),h(i),c(i),maxiter(i));
         modified = renderer(w(i),h(i),c(i),maxiter(i));
         assert(isequal(modified,original));
---
+++ CW1/q4/julia_v2.m
@@ -0,0 +1,18 @@
+function [iter,vz] = julia_v2(vz, c, maxiter)
+
+iter=ones(1,length(vz)); %define array of iterations
+
+while max(iter) < maxiter
+        mask = abs(vz) <= 1;
+        if ~any(mask)
+            break;
+        end
+        vz(mask)=vz(mask).^2 + c;
+        iter = iter + +mask;
+        
+end
+    
+       
+
+
+end
