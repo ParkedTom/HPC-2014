72eefd9bfa1a1ee8eed6158c23b6f289db89df14
Tom <tom.ed.parker@gmail.com>
2014-02-06 21:36:06 +0000

    completed 4.4

---
+++ CW2/audio/mp3_file_src.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+../local/bin/lame --silent --mp3input -t --decode  $1 -
---
+++ CW2/audio/sine_generator.c
@@ -0,0 +1,56 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+
+#include <unistd.h>
+#define SAMPLE_FREQ 44100
+#define NO_SAMPLES 512
+#define PI 3.1415927
+
+int main(int argc, char *argv[])
+{
+    
+    double freq;
+    int t = 0;
+    if(argc>1) // if an input argument is given
+    {
+        freq = atof(argv[1]); //assign to frequency
+        if(freq > (SAMPLE_FREQ/2)) // check if frequency is above nyquist frequency
+        {
+            freq = SAMPLE_FREQ/2; // set to nyquist
+        }
+    }
+    else
+    {
+        freq = 50; // if no inputs set to 50Hz
+    }
+    
+    while(1){
+        // Buffer containing one sample (left and right, both 16 bit).
+        int16_t samples[NO_SAMPLES];
+        unsigned cbBuffer=sizeof(samples);      // size in bytes of
+        
+        // Generate sine samples
+	for(int i=0; i<NO_SAMPLES - 1; i++)
+	{
+        	samples[i] = (int16_t)30000*sin((2 * PI * freq * t)/SAMPLE_FREQ);
+        	samples[i+1] = samples[i];
+        	t++;
+		i++;
+        }
+        // Copy one sample to output
+        int done=write(STDOUT_FILENO, samples, cbBuffer);
+        if(done<0){
+            fprintf(stderr, "%s : Write to stdout failed, error=%s.", argv[0], strerror(errno));
+            exit(1);
+        }else if(done!=cbBuffer){
+            fprintf(stderr, "%s : Could not read requested number of bytes from stream.\n", argv[0]);
+        }
+    }
+    
+    return 0;
+}
+
